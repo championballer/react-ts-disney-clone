84e066b36d689c4d770b47bec75c40f8dc8ee08b
Shiv Kumar <skumar8@atlassian.com>
2021-10-09 20:45:04 +0530

    Added styled components, react-router-dom and added foundation comoponents for login screen

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,37 @@
 import styled from 'styled-components';
 
-const Login = (props) => {
-    return <div>Login</div>;
-}
+const Login = (props) => 
+    <Container>
+        <Content>
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.png"/>
+            </CTA> 
+        </Content>
+    </Container>
+
+const Container = styled.section`
+    background-image: url("/images/login-background.jpg");
+    height: 100vh;
+`;
+
+const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    position: relative;
+    height: 100%;
+    padding: 80px 40px;
+`;
+
+const CTA = styled.div`
+`;
+
+const CTALogoOne = styled.img`
+    max-width: 600px;
+    padding: 12px;
+    width: 100vw;
+`;
 
 export default Login;

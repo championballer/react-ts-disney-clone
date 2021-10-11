59c880775aa6bf9275c5e7b60ab2b8c6815add38
Shiv Kumar <skumar8@atlassian.com>
2021-10-11 16:12:43 +0530

    Added description for CTA

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,6 +6,9 @@ const Login = (props) =>
             <CTA>
                 <CTALogoOne src="/images/cta-logo-one.svg"/>
                 <SignUp> GET ALL THERE </SignUp>
+                <Description>Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. 
+                    As of 26/03/21, the price of Disney+ and the Disney Bundle will increase by $1
+                </Description>
                 <CTALogoTwo src="/images/cta-logo-two.png"/>
             </CTA> 
         </Content>
@@ -18,6 +21,7 @@ const Container = styled.section`
 
 const Content = styled.div`
     display: flex;
+    flex-wrap: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
@@ -28,42 +32,45 @@ const Content = styled.div`
 `;
 
 const CTA = styled.div`
+    max-width: 800px;
     display: flex;
     flex-direction: column;
-    margin-right: auto;
-    margin-left: auto;
+    margin: auto;
     width: 100%;
     align-items: center;
 `;
 
 const CTALogoOne = styled.img`
-    max-width: 600px;
     width: 100%;
     margin-bottom: 12px;
 `;
 
 const CTALogoTwo = styled.img`
-    max-width: 600px;
     width: 100%;
 `;
 
 const SignUp = styled.a`
     display: flex;
-    max-width: 600px;
     font-weight: bold;
     color: #f9f9f9;
-    margin-bottom: 12px;
     background-color: #0063e5;
     width: 100%;
     justify-content: center;
     padding: 16.5px 0;
-    font-size: 18px;
+    font-size: 20px;
     border: 1px solid transparent;
     border-radius: 4px;
 
     &:hover {
         background-color: #0483ee;
     }
+`;
+
+const Description = styled.p`
+    text-align: center;
+    font-size : 16px;
+    letter-spacing: 1.5px;
+    line-height: 1.5;
 `;
 
 export default Login;
